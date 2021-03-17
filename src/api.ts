c9e73b537887bd06dab59a6154987b449958c779
mcpar-land <mcpar-land@users.noreply.github.com>
2021-03-17 18:14:25 +0000

    deploy: 4a6ebe5c5e324cc7fd60e86aedcb8a593fe985ff

--- src/api.ts
+++
@@ -1,5 +0,0 @@
-export function apiUrl(appId: string, extension: string): string {
-	let e = extension.charAt(0) == '/' ? extension : '/' + extension
-
-	return 'https://discord.com/api/v8/applications/' + appId + e
-}
