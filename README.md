f32a6b47cef3aa6ba895c183c259a9b4e833b9fd
mcpar-land <john@mcpar.land>
2021-03-17 14:36:42 -0500

    readme and docs update

--- README.md
+++ README.md
@@ -28,14 +28,24 @@ client.add(
 		])
 		.integer('amount', 'The amount of bagels to order', true)
 		.handler(async (client, respond, [kind, amount]) => {
-			const [edit] = await respond(
-				'You ordered ' + amount + ' ' + kind + ' bagels.'
-			)
+			await respond('You ordered ' + amount + ' ' + kind + ' bagels.')
 		})
 )
 ```
 
+Check out the small [Example Bot](/test/test-client.ts)
+
+## Scope
+
+This library isn't intended to be a replacement for comprehensive, listener-based libraries (yet?!). It's essentially a slim express webserver that responds to Discord's [outgoing webhooks](https://discord.com/developers/docs/interactions/slash-commands#receiving-an-interaction). If you want to write a bot that listens for events of any sort besides slash commands, opt for [Discord.js](https://discord.js.org/#/) instead.
+
+(Perhaps one day it will be fully featured, but that is unlikely. There are already a number of powerful libraries for making fully-fledged discord bots that will always be a better option)
+
 ## Todo
 
+There's still a lot to be done before this framework is 'production ready.'
+For now, it can respond to inputs.
+
+- [ ] Finish documenting all systems
 - [ ] Write comprehensive unit tests
 - [ ] Automatic pruning of global commands that aren't handled any more.
--- src/handler.ts
+++ src/handler.ts
@@ -3,9 +3,33 @@ import { CommandOption } from './command-types'
 import { ConvertOptionArrayToInteractionArgArray } from './types/Interaction'
 import { Response } from './types/Response'
 
+/**
+ * Handler callback, for use in [[Command.handler]]
+ * @callback
+ * @param client the client
+ * @param respond response function
+ * @param args flat array of values corresponding to the command's arguments, always in order.
+ * - [[Command.string]] - string
+ * - [[Command.integer]] - number
+ * - [[Command.boolean]] - boolean
+ * - [[Command.user]] - [[IUser]]
+ * - [[Command.role]] - [[IRole]]
+ * - [[Command.channel]] - [[IChannel]]
+ *
+ * @example
+ * ```ts
+ * command('ping', 'Respond with pong')
+ * 	.handler(async (client, respond, []) => {
+ * 		await respond("Ping pong!");
+ * 	});
+ * ```
+ */
 export type CommandHandler<T extends CommandOption<any>[]> = (
 	client: GougeClient,
 	respond: ResponseFunction,
+	/**
+	 * Hey, grat job
+	 */
 	args: ConvertOptionArrayToInteractionArgArray<T>
 ) => Promise<void>
 
@@ -13,8 +37,8 @@ export type CommandHandler<T extends CommandOption<any>[]> = (
  * Edit an existing response message
  * (Only available from [[ResponseFunction]])
  *
- * @Example
- * ``` ts
+ * @example
+ * ```ts
  * async (client, respond, [arg1, arg2]) => {
  * 	const [edit] = await respond("This will be edited right away!");
  * 	await edit("Message edited!");
@@ -26,8 +50,8 @@ export type EditResponse = (edit: Response) => Promise<void>
  * Delete an existing response message
  * (Only available from [[ResponseFunction]])
  *
- * @Example
- * ``` ts
+ * @example
+ * ```ts
  * async (client, respond, [arg1, arg2]) => {
  * 	const [edit, delete] = await respond("This will be deleted right away!");
  * 	await delete();
@@ -41,8 +65,8 @@ export type DeleteResponse = () => Promise<void>
  *
  * Returns a function that can be used to edit the new message.
  *
- * @Example
- * ``` ts
+ * @example
+ * ```ts
  * async (client, respond, [arg1, arg2]) => {
  * 	const [edit, delete, followup] = await respond("First message");
  * 	let [editFollowup] = await followup("Followup message");
